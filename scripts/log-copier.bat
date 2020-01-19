9ecd0cc98765cebc9e8f4e1fb3b3b7f79a01b978
mcdelaney <mcdelaney@gmail.com>
2020-01-19 18:20:16 -0500

    add log collector batch file

---
+++ scripts/log-copier.bat
@@ -0,0 +1,11 @@
+@echo off
+
+:CheckDCS
+echo "Copying log files..."
+CD "C:\Users\mcdel\horrible-stats"
+"python" "stats/logs_to_gs.py" --local-path "../Saved Games/DCS.openbeta_server/Logs/FrameTimeExport"  --local-suffix "*" --remote-subdir "frametime" --delete
+"python" "stats/logs_to_gs.py" --local-path "../Saved Games/DCS.openbeta_server/SlMod/Mission Stats" --local-suffix "*.lua"  --remote-subdir "mission-stats"
+"python" "stats/logs_to_gs.py" --local-path "../Documents/Tacview" --local-suffix "*.acmi" --remote-subdir "tacview"
+echo "File copy complete...waitng 5 minutes until next update..."
+timeout /t 600 /nobreak > NUL
+GOTO CheckDCS
