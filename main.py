9ba3787d71d14c174f4225cbf12c7214a1d175ee
mcdelaney <mcdelaney@gmail.com>
2020-01-20 15:41:56 -0500

    update

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from fastapi import FastAPI
 from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
+from starlette.requests import Request
 
-templates = Jinja2Templates(directory='templates')
 app = FastAPI("Stat-Server")
 app.mount("/static", StaticFiles(directory="static"), name="static")
-# app.mount("/templates", Jinja2Templatestemplates = Jinja2Templates(directory='templates'))
+templates = Jinja2Templates(directory='templates')
+
 
 @app.get("/healthz")
 def healthz():
@@ -20,6 +21,8 @@ def stats():
 
 
 @app.get("/fancy")
-def fancy():
-    context = {"data": read_stats.main(max_parse=1000).to_html(table_id="stats")}
+def fancy(request: Request):
+    df = read_stats.main(max_parse=1000)
+    context = {"request": request,
+               "data": main.to_html(table_id="stats")}
     return templates.TemplateResponse("index.html", context)
