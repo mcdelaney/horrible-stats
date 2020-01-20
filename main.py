71049efc0128df197ffb6dc497bbfe35f6f01100
mcdelaney <mcdelaney@gmail.com>
2020-01-20 16:48:46 -0500

    updates

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from starlette.templating import Jinja2Templates
 from starlette.requests import Request
 
 app = FastAPI("Stat-Server")
-app.mount("/static", StaticFiles(directory="static"), name="blobs")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory='templates')
 
 
