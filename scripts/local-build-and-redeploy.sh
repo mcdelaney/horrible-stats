38398a8222fab7b5c30967d75eec3accea02f764
mcdelaney <mcdelaney@gmail.com>
2020-01-19 19:46:47 -0500

    updates

--- scripts/local-build-and-redeploy.sh
+++ scripts/local-build-and-redeploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+IMAGE_NAME="horrible_stats"
+GCP_PROJECT_ID="dcs-analytics-257714"
+
 docker build -t gcr.io/$GCP_PROJECT_ID/$IMAGE_NAME:latest .
 docker push gcr.io/$GCP_PROJECT_ID/$IMAGE_NAME:latest
-kubectl delete deployment horrible-stats --namespace horrible-stats
-kubectl apply -f deployment.yaml
+# kubectl delete deployment horrible-stats --namespace horrible-stats
+# kubectl apply -f deployment.yaml
