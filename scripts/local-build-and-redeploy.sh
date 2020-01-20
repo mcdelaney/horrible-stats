dd23ebe5af24864387f2bb9dbfbc89e8171bfcaf
mcdelaney <mcdelaney@gmail.com>
2020-01-20 09:06:53 -0500

    Update deployment scripts

--- scripts/local-build-and-redeploy.sh
+++ scripts/local-build-and-redeploy.sh
@@ -1,16 +1,22 @@
-<<<<<<< HEAD
 #!/usr/bin/env bash
-docker build -t gcr.io/$GCP_PROJECT_ID/$IMAGE_NAME:latest .
-docker push gcr.io/$GCP_PROJECT_ID/$IMAGE_NAME:latest
-kubectl delete deployment horrible-stats --namespace horrible-stats
-kubectl apply -f deployment.yaml
-=======
-#!/usr/bin/env bash
+
 IMAGE_NAME="horrible_stats"
 GCP_PROJECT_ID="dcs-analytics-257714"
+TAG=$(git log --pretty=format:'%h' -n 1)
 
+echo "Building and deploying $IMAGE_NAME at hash $TAG..."
+
+echo "Building new image..."
 docker build -t gcr.io/$GCP_PROJECT_ID/$IMAGE_NAME:latest .
+
+echo "Setting commit hash tag on latest image..."
+docker tag gcr.io/$GCP_PROJECT_ID/$IMAGE_NAME:latest gcr.io/$GCP_PROJECT_ID/$IMAGE_NAME:$TAG
+
+echo "Pushing latest image to GCR..."
 docker push gcr.io/$GCP_PROJECT_ID/$IMAGE_NAME:latest
-# kubectl delete deployment horrible-stats --namespace horrible-stats
-# kubectl apply -f deployment.yaml
->>>>>>> 0c195b62f565aa1bc5a72b105cebd887b8487d90
+
+echo "Pushing commit hash image to GCR..."
+docker push gcr.io/$GCP_PROJECT_ID/$IMAGE_NAME:$TAG
+
+echo "Deploying new image to prod..."
+kubectl set image deployment horrible-stats app=gcr.io$/$GCP_PROJECT_ID/$IMAGE_NAME:$TAG -n horrible-stats
