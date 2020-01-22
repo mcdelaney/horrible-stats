02030a90db2f272b0c1661110642775fb4703bf3
mcdelaney <mcdelaney@gmail.com>
2020-01-21 22:34:28 -0500

    update

--- bin/logs-export.bat
+++ bin/logs-export.bat
@@ -1,8 +1,8 @@
 
 CD "C:\Users\mcdel\horrible-stats"
 
-"python" "stats/logs_to_gs.py" --local-path "../Saved Games/DCS.openbeta_server/Logs/FrameTimeExport"  --local-suffix "*" --remote-subdir "frametime" --delete
+"python" "horrible/stats/logs_to_gs.py" --local-path "../Saved Games/DCS.openbeta_server/Logs/FrameTimeExport"  --local-suffix "*" --remote-subdir "frametime" --delete
 
-"python" "stats/logs_to_gs.py" --local-path "../Saved Games/DCS.openbeta_server/SlMod/Mission Stats" --local-suffix "*.lua"  --remote-subdir "mission-stats"
+"python" "horrible/stats/logs_to_gs.py" --local-path "../Saved Games/DCS.openbeta_server/SlMod/Mission Stats" --local-suffix "*.lua"  --remote-subdir "mission-stats"
 
-"python" "stats/logs_to_gs.py" --local-path "../Documents/Tacview" --local-suffix "*.acmi" --remote-subdir "tacview"
+"python" "horrible/stats/logs_to_gs.py" --local-path "../Documents/Tacview" --local-suffix "*.acmi" --remote-subdir "tacview"
