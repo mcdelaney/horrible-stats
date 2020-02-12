42ac877cc3518c1457767420f741dac1e4dc1039
mcdelaney <mcdelaney@gmail.com>
2020-02-12 14:35:07 -0500

    update paths for log mgmt

--- scripts/log-copier.bat
+++ scripts/log-copier.bat
@@ -2,10 +2,10 @@
 
 :CheckDCS
 echo "Copying log files..."
-CD "C:\Users\mcdel\horrible-stats\horrible"
-"python" "stats/logs_to_gs.py" --local-path "../../Saved Games/DCS.openbeta_server/Logs/FrameTimeExport"  --local-suffix "*" --remote-subdir "frametime" --delete
-"python" "stats/logs_to_gs.py" --local-path "../../Saved Games/DCS.openbeta_server/SlMod/Mission Stats" --local-suffix "*.lua"  --remote-subdir "mission-stats"
-"python" "stats/logs_to_gs.py" --local-path "../../Documents/Tacview" --local-suffix "*" --remote-subdir "tacview"
+CD "C:\Users\mcdel\horrible-stats"
+"python" "scripts/logs_to_gs.py" --local-path "../../Saved Games/DCS.openbeta_server/Logs/FrameTimeExport"  --local-suffix "*" --remote-subdir "frametime" --delete
+"python" "scripts/logs_to_gs.py" --local-path "../../Saved Games/DCS.openbeta_server/SlMod/Mission Stats" --local-suffix "*.lua"  --remote-subdir "mission-stats"
+"python" "scripts/logs_to_gs.py" --local-path "../../Documents/Tacview" --local-suffix "*" --remote-subdir "tacview"
 echo "File copy complete...waitng 5 minutes until next update..."
 timeout /t 600 /nobreak > NUL
 GOTO CheckDCS
