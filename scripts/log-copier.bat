1a0f9a9144b7d3d483990f83475acf4e02dce375
mcdelaney <mcdelaney@gmail.com>
2020-02-18 10:20:44 -0500

    copy mission events to gcs

--- scripts/log-copier.bat
+++ scripts/log-copier.bat
@@ -5,6 +5,7 @@ echo "Copying log files..."
 CD "C:\Users\mcdel\horrible-stats"
 "python" "scripts/logs_to_gs.py" --local-path "C:\Users\mcdel\Saved Games/DCS.openbeta_server/Logs/FrameTimeExport"  --local-suffix "*" --remote-subdir "frametime" --delete
 "python" "scripts/logs_to_gs.py" --local-path "C:\Users\mcdel\Saved Games\DCS.openbeta_server\SlMod\Mission Stats" --local-suffix "*.lua"  --remote-subdir "mission-stats"
+"python" "scripts/logs_to_gs.py" --local-path "C:\Users\mcdel\Saved Games\DCS.openbeta_server\SlMod\Mission Events" --local-suffix "*.txt"  --remote-subdir "mission-events"
 "python" "scripts/logs_to_gs.py" --local-path "C:\Users\mcdel\Documents\Tacview" --local-suffix "*" --remote-subdir "tacview"
 echo "File copy complete...waitng 5 minutes until next update..."
 timeout /t 600 /nobreak > NUL
