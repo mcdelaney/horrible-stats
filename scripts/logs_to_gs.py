ac4d7ea5a77d9ae05211c35b8b2322122c657eca
mcdelaney <mcdelaney@gmail.com>
2020-03-15 16:37:40 +0100

    enable multipart upload

--- scripts/logs_to_gs.py
+++ scripts/logs_to_gs.py
@@ -5,8 +5,9 @@ import logging
 from pathlib import Path
 import urllib.parse
 
+from google.auth.transport.requests import AuthorizedSession
+from google.resumable_media import requests, common
 from google.cloud import storage
-
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -14,6 +15,84 @@ log.setLevel(level=logging.INFO)
 consoleHandler = logging.StreamHandler()
 log.addHandler(consoleHandler)
 
+
+
+class GCSObjectStreamUpload(object):
+    def __init__(
+            self, 
+            client: storage.Client,
+            bucket_name: str,
+            blob_name: str,
+            chunk_size: int=256 * 1024
+        ):
+        self._client = client
+        self._bucket = self._client.bucket(bucket_name)
+        self._blob = self._bucket.blob(blob_name)
+
+        self._buffer = b''
+        self._buffer_size = 0
+        self._chunk_size = chunk_size
+        self._read = 0
+
+        self._transport = AuthorizedSession(
+            credentials=self._client._credentials
+        )
+        self._request = None  # type: requests.ResumableUpload
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, *_):
+        if exc_type is None:
+            self.stop()
+
+    def start(self):
+        url = (
+            f'https://www.googleapis.com/upload/storage/v1/b/'
+            f'{self._bucket.name}/o?uploadType=resumable'
+        )
+        self._request = requests.ResumableUpload(
+            upload_url=url, chunk_size=self._chunk_size
+        )
+        self._request.initiate(
+            transport=self._transport,
+            # content_type='application/octet-stream',
+            content_type='text/plain',
+            stream=self,
+            stream_final=False,
+            metadata={'name': self._blob.name},
+        )
+
+    def stop(self):
+        self._request.transmit_next_chunk(self._transport)
+
+    def write(self, data: bytes) -> int:
+        data_len = len(data)
+        self._buffer_size += data_len
+        self._buffer += data
+        del data
+        while self._buffer_size >= self._chunk_size:
+            try:
+                self._request.transmit_next_chunk(self._transport)
+            except common.InvalidResponse:
+                self._request.recover(self._transport)
+        return data_len
+
+    def read(self, chunk_size: int) -> bytes:
+        # I'm not good with efficient no-copy buffering so if this is
+        # wrong or there's a better way to do this let me know! :-)
+        to_read = min(chunk_size, self._buffer_size)
+        memview = memoryview(self._buffer)
+        self._buffer = memview[to_read:].tobytes()
+        self._read += to_read
+        self._buffer_size -= to_read
+        return memview[:to_read].tobytes()
+
+    def tell(self) -> int:
+        return self._read
+    
+    
 
 async def upload_files(local_path_glob, remote_subdir: str, delete_files: bool):
     """Upload files to GCP bucket."""
@@ -39,16 +118,36 @@ async def upload_files(local_path_glob, remote_subdir: str, delete_files: bool):
                     log.info("File not compressed...zipping...")
                     content = gzip.compress(content)
                 log.info("Compression complete...uploading...")
-                blob.content_type = "text/plain"
-                blob.content_encoding = "gzip"
-                tries = 0
-                while tries <= 2:
-                    try:
-                        blob.upload_from_string(content)
-                        break
-                    except Exception as err:
-                        log.error(err)
-                        tries += 1
+                
+                start_byte = 0
+                end_byte = 0
+                i = 0
+
+                with GCSObjectStreamUpload(client=client, bucket_name='horrible-server', blob_name=gs_filename) as s:
+                    chunks = len(content) // s._chunk_size
+                    log.info('Starting upload...')
+                    
+                    end_byte += s._chunk_size
+                    data = content[start_byte:end_byte]
+                    while data:
+                        log.info(f'Writing chunk {i} of {chunks}...')
+                        s.write(data)
+                        
+                        start_byte = end_byte
+                        end_byte += s._chunk_size
+                        i += 1
+                        data = content[start_byte:end_byte]                        
+
+                # blob.content_type = "text/plain"
+                # blob.content_encoding = "gzip"
+                # tries = 0
+                # while tries <= 2:
+                #     try:
+                #         blob.upload_from_string(content)
+                #         break
+                #     except Exception as err:
+                #         log.error(err)
+                #         tries += 1
 
                 if delete_files:
                     log.info("File uploaded...deleting...")
