3d29015866fe3c62ca261fa00c4ae0fcc91613d5
mcdelaney <mcdelaney@gmail.com>
2020-03-24 17:06:54 -0600

    rename cluster config

--- scripts/configure-local-cluster.sh
+++ scripts/configure-local-cluster.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-SECRETNAME="gcr-auth"
 
+SECRETNAME="gcr-auth"
 kubectl create secret docker-registry $SECRETNAME \
   --docker-server=https://gcr.io \
   --docker-username=_json_key \
@@ -10,3 +10,5 @@ kubectl create secret docker-registry $SECRETNAME \
 kubectl create secret generic dcs-storage-gcs --from-file dcs-storage-gcs.json
 
 kubectl apply -f scripts/gcr-pull-local-svc-acct.yaml
+kubectl create namespace horrible-stats
+kubectl config set-context --current --namespace horrible-stats
