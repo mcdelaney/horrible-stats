58ed20952ba64f032dbcba5f909ca0e946331a47
mcdelaney <mcdelaney@gmail.com>
2020-04-04 09:49:41 -0600

    update deployments + add ssl ingress configuration

--- scripts/configure-local-cluster.sh
+++ scripts/configure-local-cluster.sh
@@ -3,11 +3,7 @@ echo "Creating registry..."
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 SECRETNAME="gcr-auth"
-kubectl create secret docker-registry gcr-auth \
-  --docker-server=https://gcr.io \
-  --docker-username=_json_key \
-  --docker-email=mcdelaney@gmail.com \
-  --docker-password="$(cat dcs-storage-gcs.json)"
+kubectl create secret docker-registry "gcr-auth" --docker-server=https://gcr.io --docker-username=_json_key --docker-email=mcdelaney@gmail.com --docker-password="$(cat dcs-storage-gcs.json)"
 
 kubectl create secret generic dcs-storage-gcs --from-file dcs-storage-gcs.json
 
