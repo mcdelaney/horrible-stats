ed56ab76f03eb14a7163a30322c1543e75b88fb5
mcdelaney <mcdelaney@gmail.com>
2021-01-23 17:10:26 -0700

    fix cert manager

--- scripts/cluster-mgmt/install-cert-manager.sh
+++ scripts/cluster-mgmt/install-cert-manager.sh
@@ -2,7 +2,7 @@
 
 kubectl create namespace cert-manager
 
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.13.0/cert-manager.yaml
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
 
 kubectl create clusterrolebinding cluster-admin-binding \
   --clusterrole=cluster-admin \
